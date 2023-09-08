ea555a742639c6b91120e2bbc47308ffe5470ba4
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-07 23:30:03 -0400

    start bubble sort

---
+++ src/main/java/com/hyunwoocho/beginningJavaDS/ch2/practice/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.hyunwoocho.beginningJavaDS.ch2.practice;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public int[] sort(int[] numbers) {
+
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j + 1];
+                    numbers[j + 1] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+        return numbers;
+    }
+
+}
---
+++ src/test/java/com/hyunwoocho/beginningJavaDS/ch2/practice/BubbleSortTest.java
@@ -0,0 +1,15 @@
+package com.hyunwoocho.beginningJavaDS.ch2.practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    void sort() {
+        BubbleSort bubbleSort = new BubbleSort();
+//        assertEquals(new int[] {1, 2, 3}, bubbleSort.sort(new int[] {2, 3, 1}));
+    }
+
+}
